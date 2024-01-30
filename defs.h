60357a51dd771be82911118312cdb32e1e8cce62
adam <adguscott@outlook.com>
2024-01-30 06:33:59 +0000

    Modified SCREEN_HEIGHT and PLAYER_SPEED, added TILESIZE

--- defs.h
+++ defs.h
@@ -1,4 +1,5 @@
 #define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 720
+#define SCREEN_HEIGHT 704
+#define TILESIZE 64
 #define SPRITES_DIR "/data/sprite_mapping/"
-#define PLAYER_SPEED 5
+#define PLAYER_SPEED 4
