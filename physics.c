9e7e3474222bcca648c7b805b7e504f277da2674
adam <adguscott@outlook.com>
2024-01-31 19:33:44 +0000

    Added collision, playerMoving and entityMoving functions

--- physics.c
+++ physics.c
@@ -1,5 +1,5 @@
+#include "common.h"
 #include "physics.h"
-#include "defs.h"
 
 void centreToTile(int *x, int *y, int width, int height, int nx, int ny)
 {
@@ -17,3 +17,20 @@ int getCentre(int dim)
 
     return (TILESIZE - dim) / 2;
 }
+
+// stolen from parallelrealities.co.uk
+bool collision(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2)
+{
+	return (MAX(x1, x2) < MIN(x1 + w1, x2 + w2)) && (MAX(y1, y2) < MIN(y1 + h1, y2 + h2));
+}
+
+bool playerMoving(Player *player)
+{
+    return player->x != player->nx || player->y != player->ny;
+}
+
+bool entityMoving(Entity *entity)
+{
+    return entity->x != entity->nx || entity->y != entity->ny;
+}
+
--- physics.h
+++ physics.h
@@ -1,2 +1,5 @@
+bool collision(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2);
 void centreToTile(int *x, int *y, int width, int height, int nx, int ny);
 int getCentre(int dim);
+bool playerMoving(Player *player);
+bool entityMoving(Entity *entity);
