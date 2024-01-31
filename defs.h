e94a6a4728452faca8f52ebffd717c74dc5e770e
adam <adguscott@outlook.com>
2024-01-31 19:32:21 +0000

    Added Min and Max functions and entity flags

--- defs.h
+++ defs.h
@@ -3,3 +3,9 @@
 #define TILESIZE 64
 #define SPRITES_DIR "/data/sprite_mapping/"
 #define PLAYER_SPEED 4
+#define MAX(a,b) ((a) > (b) ? (a) : (b))
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
+
+// entity flags
+#define IS_SOLID 1
+#define IS_PUSHABLE 2
