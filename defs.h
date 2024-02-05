3b4bbe1d9738aaa32e598ed79f06913a2fca1955
adam <adguscott@outlook.com>
2024-02-05 13:43:27 +0000

    Added more flags for entities

--- defs.h
+++ defs.h
@@ -1,5 +1,5 @@
-#define SCREEN_WIDTH 1280
-#define SCREEN_HEIGHT 704
+#define SCREEN_WIDTH 512
+#define SCREEN_HEIGHT 576
 #define TILESIZE 64
 #define SPRITES_DIR "/data/sprite_mapping/"
 #define PLAYER_SPEED 4
@@ -9,3 +9,7 @@
 // entity flags
 #define IS_SOLID 1
 #define IS_PUSHABLE 2
+#define IS_SWITCH 4
+#define IS_RED 8
+#define IS_BLUE 16
+#define IS_GREEN 32
