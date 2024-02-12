0bc8cd4a4c7d3862f508be5e3e5a862d83e7a218
Adam Guscott <adguscott@outlook.com>
2024-02-12 20:31:10 +0000

    Formatting

--- draw.c
+++ draw.c
@@ -3,20 +3,20 @@
 
 extern Game game;
 
-void blitRect(SDL_Texture *texture, Sprite *src, int x, int y)
+void blitRect(SDL_Texture* texture, Sprite* src, int x, int y)
 {
-    SDL_Rect source, dest;
-    
-    source.x = src->x;
-    source.y = src->y;
-    source.w = src->w;
-    source.h = src->h;
+	SDL_Rect source, dest;
 
-    dest.x = x;
-    dest.y = y;
-    dest.w = src->w;
-    dest.h = src->h;
+	source.x = src->x;
+	source.y = src->y;
+	source.w = src->w;
+	source.h = src->h;
 
-    SDL_RenderCopy(game.renderer, texture, &source, &dest);
+	dest.x = x;
+	dest.y = y;
+	dest.w = src->w;
+	dest.h = src->h;
+
+	SDL_RenderCopy(game.renderer, texture, &source, &dest);
 }
 
